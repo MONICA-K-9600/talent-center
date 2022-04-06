9e5f315a6d2d11f8366f59941b9bc1fa863f29ee
HP <monicamonisa2@gmail.com>
2022-04-06 15:42:45 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { MainViewComponent } from "./main-view/main-view.component";
 const router: Routes=[
-    {path:'',component:MainViewComponent}
 ];
 @NgModule({
     imports:[RouterModule.forRoot(router)],
